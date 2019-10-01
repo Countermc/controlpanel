605f3c6b2ad9f9ef505d62da72f82f9aa5989819
countermc <countermc@gmail.com>
2019-10-01 11:27:29 +0200

    Eliminado del repositorio

--- virtualserver/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\informatico3\documents\python\django\virtualserver\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
