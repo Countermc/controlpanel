ed5b30533fff13df9f6b4b32fb83f4c3a2166956
countermc <countermc@gmail.com>
2019-10-01 09:58:07 +0200

    v1.01

---
+++ virtualserver/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\informatico3\documents\python\django\virtualserver\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
